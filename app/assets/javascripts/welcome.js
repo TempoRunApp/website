81d5ddcbfe270bdce7feb89a52941c3c1d19d589
Grayson Wright <wright.grayson@gmail.com>
2013-05-20 16:23:25 +0800

    Fix paragraph display bug caused by inline js
    Move offending JS into
    app/assets/javascripts/welcome.js

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -10,11 +10,25 @@ $(document).ready(function() {
   });
 });
 
+$(document).ready(function() {
+  var d = document
+  var s = "script"
+  var id= "twitter-wjs"
+  var js,fjs=d.getElementsByTagName(s)[0];
+  if(!d.getElementById(id)) {
+    js=d.createElement(s);
+    js.id=id;
+    js.src="//platform.twitter.com/widgets.js";
+    fjs.parentNode.insertBefore(js,fjs);
+  }
+});
+
+
 /* In order for box shadows on each section to work properly,
  * the sections need to be placed in descending z-index order. */
 $(document).ready(function() {
   $('.section').each(function(index) {
-        zindex = index * -1;
-           $(this).css('zIndex', zindex);
+    zindex = index * -1;
+    $(this).css('zIndex', zindex);
   });
 });
