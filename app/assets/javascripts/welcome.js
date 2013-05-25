788523abb8d97dbb878f9cdee79ec393937ab881
Grayson Wright <wright.grayson@gmail.com>
2013-05-25 09:59:52 +0800

    Make product video responsive
    Javascript solution, found at:
    http://stackoverflow.com/questions/7737557#answer-12866709

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,3 +1,4 @@
+/* Navigation Bar */
 $(document).ready(function() {
   $('#nav').scrollspy({
     min: $('#nav').offset().top + 10,
@@ -10,6 +11,19 @@ $(document).ready(function() {
   });
 });
 
+/* Video Aspect Ratio */
+jQuery(function() {
+  function setAspectRatio() {
+    jQuery('iframe').each(function() {
+      jQuery(this).css('height',jQuery(this).width()*9/16);
+    });
+  }
+
+  setAspectRatio();
+  jQuery(window).resize(setAspectRatio);
+});
+
+/* Twitter Feed */
 $(document).ready(function() {
   var d = document
   var s = "script"
